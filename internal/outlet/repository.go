4fc1f5d09d2fdcb3c10744af675865a036d4ab60
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-12-10 11:37:31 +0700

    crud merchant done

--- internal/outlet/repository.go
+++ internal/outlet/repository.go
@@ -16,59 +16,65 @@ type OutletRepository interface {
 	Update(body Outlet) error
 }
 
-type mstMtrRepository struct {
+type outletRepository struct {
 	conn *gorm.DB
 }
 
 func NewOutletRepository(conn *gorm.DB) OutletRepository {
-	return &mstMtrRepository{
+	return &outletRepository{
 		conn: conn,
 	}
 }
 
-func (lR *mstMtrRepository) DetailOutlet(id string) Outlet {
-	mstMtr := Outlet{ID: id}
-	lR.conn.Find(&mstMtr)
-	return mstMtr
+func (lR *outletRepository) DetailOutlet(id string) Outlet {
+	outlet := Outlet{ID: id}
+	lR.conn.Preload("MediaPromosi").Find(&outlet)
+	return outlet
 }
 
-func (lR *mstMtrRepository) CreateOutlet(data Outlet) error {
-	result := lR.conn.Create(&data)
+func (lR *outletRepository) CreateOutlet(data Outlet) error {
+	mediaPromosi := data.MediaPromosi
+	result := lR.conn.Omit("MediaPromosi").Save(&data)
 	if result.Error != nil {
 		fmt.Println("ini error ", result.Error)
 		return result.Error
-	} else {
-		return nil
 	}
-
+	err := lR.conn.Model(&data).Association("MediaPromosi").Replace(mediaPromosi)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (lR *mstMtrRepository) Update(data Outlet) error {
+func (lR *outletRepository) Update(data Outlet) error {
 	record := Outlet{ID: data.ID}
 	lR.conn.Find(&record)
+	mediaPromosi := data.MediaPromosi
 	if record.Nama == "" {
 		return errors.New("maaf data tidak ada")
 	}
-	result := lR.conn.Save(&data)
-	// lR.Model(&user).Association("Roles").Replace(newRoles)
+	result := lR.conn.Omit("MediaPromosi").Save(&data)
 	if result.Error != nil {
 		fmt.Println("ini error ", result.Error)
 		return result.Error
-	} else {
-		return nil
 	}
+	err := lR.conn.Model(&record).Association("MediaPromosi").Replace(mediaPromosi)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (lR *mstMtrRepository) MasterData(search string, limit int, pageParams int) []Outlet {
-	mstMtr := []Outlet{}
-	query := lR.conn.Where("kd_mdl like ? or nm_mtr like ? or no_mtr like ? or merk like ? ", "%"+search+"%", "%"+search+"%", "%"+search+"%", "%"+search+"%")
-	query.Scopes(utils.Paginate(&utils.PaginateParams{PageParams: pageParams, Limit: limit})).Find(&mstMtr)
-	return mstMtr
+func (lR *outletRepository) MasterData(search string, limit int, pageParams int) []Outlet {
+	outlet := []Outlet{}
+	query := lR.conn.Where("nama like ? or alamat like  ? ", "%"+search+"%", "%"+search+"%")
+	query.Scopes(utils.Paginate(&utils.PaginateParams{PageParams: pageParams, Limit: limit})).Find(&outlet)
+	return outlet
 }
 
-func (lR *mstMtrRepository) MasterDataCount(search string) int64 {
-	var mstMtr []Outlet
-	query := lR.conn.Where("kd_mdl like ? or nm_mtr like ? or no_mtr like ? or merk like ? ", "%"+search+"%", "%"+search+"%", "%"+search+"%", "%"+search+"%")
-	query.Select("no_mtr").Find(&mstMtr)
-	return int64(len(mstMtr))
+func (lR *outletRepository) MasterDataCount(search string) int64 {
+	var outlet []Outlet
+	query := lR.conn.Where("nama like ? or alamat like  ? ", "%"+search+"%", "%"+search+"%")
+	query.Select("no_mtr").Find(&outlet)
+	return int64(len(outlet))
 }
